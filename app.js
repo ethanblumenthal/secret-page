d8a72d5a1dab10b83e81378bffee019b05d6ddc1
Ethan Blumenthal <ethan.blumenthal@gmail.com>
2017-12-18 10:14:19 -0800

    added secret page

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/', function(req, res) {
     res.render('home');
 });
 
+app.get('/secret', function(req,res) {
+    res.render('secret');
+});
+
 app.listen(3000, function() {
     console.log('Serving on localhost:3000');
 });
